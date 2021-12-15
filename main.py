473d77efb8d56062703afd12afe05c9a4b58b491
georgewang0608 <yunqiw68@uw.edu>
2021-12-14 18:10:43 -0800

    add

--- main.py
+++ main.py
@@ -23,11 +23,27 @@ def appdetails(appid, filters = ''):
     data = json.loads(safe_get(url))
     return data
 
-def featuredcategories(genre) {
-    url = baseurl + "featuredcategories"
-    
+def recommendation(genre):
+    dic = {}
+    url = baseurl + "featured/"
+    data = json.loads(safe_get(url))
+    dic = recommendation_helper(data, genre, dic, "large_capsules")
+    dic = recommendation_helper(data, genre, dic, "featured_win")
+    dic = recommendation_helper(data, genre, dic, "featured_mac")
+    dic = recommendation_helper(data, genre, dic, "featured_linux")
+    return dic
 
-}
+
+
+
+def recommendation_helper(data, genre, dic, OS):
+    for x in data[OS]:
+        appdetail = appdetails(x["id"])
+        genres = appdetail[str(x["id"])]["data"]["genres"]     
+        for y in genres:
+            if genre == y["description"]:
+                dic[str(x["id"])] = {"name": x["name"], "genre": genre}           
+    return dic
 
 app = Flask(__name__)
 
@@ -39,14 +55,12 @@ def main_handler():
 @app.route('/homepage')
 def homepage_handler():
     username = request.args.get('username')
-    if(get_games(username) == None) {
-        return render_template("homepage.html", page_title='honepage'， name=username)
-    } else {
+    if get_games(username) == None: 
+        return render_template("homepage.html", page_title='honepage', name=username)
+    else:
         return render_template("mainpage.html", page_title="mainpage - Error", prompt = "The steam ID is either invalid or private")
-    }
+    
 
 
 if __name__ == '__main__':
-    app.run(host="georges-MacBook-Pro.local", port=8080, debug=True)
-
-print(pretty(appdetails("1506440","")))
+    app.run(host="georges-MacBook-Pro.local", port=8080, debug=True)    
