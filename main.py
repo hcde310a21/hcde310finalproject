783b8e4e11023d01579986c74891d8c14f807349
genejwang <mk6E85p55pNk>
2021-12-15 04:16:31 -0800

    functions edited and tentatively completed

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import urllib.request, urllib.error, urllib.parse, json, webbrowser
 from flask import Flask, render_template, request
 
-baseurl = "http://store.steampowered.com/api/"
+storeurl = "http://store.steampowered.com/api/"
+steamurl = "https://api.steampowered.com/"
 
+# Given method to make JSON more readable. Requires JSON object as parameter and returns a readable
+# version of it to print
 def pretty(obj):
     return json.dumps(obj, sort_keys=True, indent=2)
 
+# Given method to check for errors on a given URL. Requires a URL and either returns an opened
+# request to read it or None if it reaches an error
 def safe_get(url):
     try:
         return urllib.request.urlopen(url).read()
@@ -17,24 +22,68 @@ def safe_get(url):
         print("Reason: ", e.reason)
     return None
 
+# Uses storefront API to access details about a particular game. Requires the game's appid
+# and any desired filters (optional) as parameters and returns string of the json format.
 def appdetails(appid, filters = ''):
     params = {"appids": appid, "filters": filters}
-    url = baseurl + "appdetails?" + urllib.parse.urlencode(params)
-    data = json.loads(safe_get(url))
-    return data
+    url = storeurl + "appdetails?" + urllib.parse.urlencode(params)
+    safedata = safe_get(url)  # split this up in case invalid appid so we can get an error
+    loadeddata = json.loads(safedata)  # why is this json.loads instead of json.load?
+    return loadeddata
 
+# Accesses personal Steam library by constructing url and returning a json file. Requires an
+# authentication key and a Steam user's personal Steam ID. Can also exclude free games if desired.
+# Temporarily commented out because we don't appear to be using it right now.
+#def get_games(key, steamid, include_played_free_games=True, appids_filter=None):
+#    combinedurl = steamurl + "IPlayerService/GetOwnedGames/v1/?key=" + key + "&steamid=" + \
+#                  str(steamid) + "&include_appinfo=" + str(True) + "&nclude_played_free_games=" \
+#                  + str(include_played_free_games) + "&appids_filter=" + str(appids_filter)
+#    safedata = safe_get(combinedurl)
+#    return json.load(safedata)
+
+# Accesses user's history to check recent games played (default of 10 games) and returns a json file
+# about those games' data.
+def get_games_recent(key, steamid, count=10):
+    combinedurl = steamurl + "IPlayerService/GetRecentlyPlayedGames/v1/?key=" + key + "&steamid="\
+                  + str(steamid) + "count=" + str(count)
+    safedata = safe_get(combinedurl)
+    return json.load(safedata)
+
+# Accesses a game's info to check the number of players currently online on a particular game,
+# taking the game's appid as a parameter and return a json of the info. Temporarily commented out
+# because we don't appear to be using it at the moment.
+#def get_current_players(appid):
+#    combinedurl = steamurl + "ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=" + str(appid)
+#    safedata = safe_get(combinedurl)
+#    return json.load(safedata)
+
+# Searches for streams on Twitch using a search keyword (in our case, the name of a game), which
+# is taken as a parameter. As of right now, no way to organize search results or check if stream is
+# accurate. Currently returning list of 3 stream IDs for embedding onto site but can be changed.
+def get_twitch_search(game):
+    url = "https://api.twitch.tv/helix/search/categories?query=" + game
+    hdr = {'Client-ID': "10thxh1tgtgb7j2g4842qcgptipob4",
+                 'Authorization': "Bearer rmxafdqguk8gmu9h6jnl512zznpj3e"}
+    req = urllib.request.Request(url, headers=hdr)
+    response = urllib.request.urlopen(req)
+    response = response.read().decode('utf-8')
+    streamdata = json.loads(response)
+    streamlist = []
+    for stream in streamdata["data"][0:3]: # can fetch more/less than 3 streams if desired
+        streamlist.append(stream["id"])
+    return streamlist
+
+# Takes in a genre/tag as a parameter to search the Steam featured section for any similar games
+# and returns it as a dictionary.
 def recommendation(genre):
     dic = {}
-    url = baseurl + "featured/"
+    url = storeurl + "featured/"
     data = json.loads(safe_get(url))
     dic = recommendation_helper(data, genre, dic, "large_capsules")
     dic = recommendation_helper(data, genre, dic, "featured_win")
     dic = recommendation_helper(data, genre, dic, "featured_mac")
     dic = recommendation_helper(data, genre, dic, "featured_linux")
     return dic
-
-
-
 
 def recommendation_helper(data, genre, dic, OS):
     for x in data[OS]:
@@ -44,6 +93,28 @@ def recommendation_helper(data, genre, dic, OS):
             if genre == y["description"]:
                 dic[str(x["id"])] = {"name": x["name"], "genre": genre}           
     return dic
+
+# Takes in two dictionaries, one with a player's recently played games and another with any
+# recommendations they've been given. Pulls news and stream data into dictionary separated by
+# recent and recommended for return. This info should go directly onto our page.
+def get_info(recentdict, recsdict):
+    infodict = {"recent games":[], "recommended games":[]}
+    for game in recentdict["responses"]["games"]: # call must pass in recentdict with get_recent_games
+        combinedurl = steamurl + "ISteamNews/GetNewsForApp/v2/?appid=" + str(game["appid"]) # can add max length to content or count for # of posts
+        safedata = safe_get(combinedurl)
+        news = json.load(safedata)
+        streams = get_twitch_search(game["name"])
+        infodict["recent games"].append({"name": game["name"],
+                                         "news": news["appnews"]["newsitems"], "streams": streams})
+    for gameid in recsdict:
+        combinedurl = steamurl + "ISteamNews/GetNewsForApp/v2/?appid=" + str(gameid) # can add max length to content or count for # of posts
+        safedata = safe_get(combinedurl)
+        news = json.load(safedata)
+        streams = get_twitch_search(gameid["name"])
+        infodict["recommended games"].append({"name": gameid["name"], "genrerec": gameid["genre"],
+                                              "news": news["appnews"]["newsitems"],
+                                              "streams": streams})
+    return infodict
 
 app = Flask(__name__)
 
